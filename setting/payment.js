5a4106ea9e890fc959abb26c9233acff97dae63b
hilmiganzbot12 <96254454+hilmiganzbot12@users.noreply.github.com>
2022-03-24 21:53:48 +0700

    Update payment.js

--- setting/payment.js
+++ setting/payment.js
@@ -1,8 +1,8 @@
 exports.payment = (prefix) => {
 return `╭──「 *PAYMENT / DONASI* 」
 │
-├❏ Gopay : _Tuh Kak Scan Qrnya_
-├❏ Dana : _081221837415_
+├❏ Gopay : _082143349069_
+├❏ Dana : _0821143349069_
 ├❏ Bank BRI : _3653 0103 1536 539_
 │
 ╰──`
